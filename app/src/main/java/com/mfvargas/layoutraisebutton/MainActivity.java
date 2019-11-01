cc8949124da9c70ab6ab9cc07d8d619f55c516ec
Mafe0394 <mfvargasm0394@gmail.com>
2019-10-31 20:21:24 -0500

    Sin libreria de compatibilidad de material design

--- app/src/main/java/com/mfvargas/layoutraisebutton/MainActivity.java
+++ app/src/main/java/com/mfvargas/layoutraisebutton/MainActivity.java
@@ -3,12 +3,42 @@ package com.mfvargas.layoutraisebutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFAB();
+    }
+
+    public void agregarFAB(){
+        FloatingActionButton miFAB =findViewById(R.id.miFAB);
+        miFAB.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        //Toast.makeText(this,getResources().getString(R.string.app_name),Toast.LENGTH_SHORT).show();
+        Snackbar.make(v,
+                getResources().getString(R.string.app_name),
+                Snackbar.LENGTH_SHORT)
+                .setAction(R.string.texto_accion, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getBaseContext(), getResources().getString(R.string.app_name),Toast.LENGTH_SHORT).show();
+                        Log.d("Gato","Azul");
+                    }
+                })
+                .setActionTextColor(getResources().getColor(R.color.colorPrimaryDark))
+                .show();
     }
 }
